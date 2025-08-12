7d5284eac02b6795822a47ff16a41b2bc28ce6fe
Matheus Rios <matheus.barrosorios@gmail.com>
2025-08-12 20:55:12 -0300

    Add comments

--- gameClasses.js
+++ gameClasses.js
@@ -1,17 +1,21 @@
 const readline = require('readline');
 
+// Create a readline interface for input/output
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// Define the Game class
 class Game {
+    // Constructor to initialize character properties
     constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
     }
 
+    // Method to get character information and determine attack type
     getInfo() {
         let ataque;
         switch (this.tipo) {
@@ -37,12 +41,14 @@ class Game {
         };
     }
 
+    // Method to simulate an attack
     atacar() {
         let tipo = this.getInfo();
         console.log(`O ${this.tipo} atacou usando ${tipo.ataque}`);
     }
 }
 
+// Prompt the user for character details
 rl.question("Qual tipo de personagem você deseja criar? (magico, guerreiro, monge, ninja) ", (tipo) => {
     rl.question("Qual nome do personagem? ", (nome) => {
         rl.question("Qual a idade do personagem? ", (idade) => {
